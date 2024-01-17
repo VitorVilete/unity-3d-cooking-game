fb938e13acadebb938193cc9e316a442ea4fb801
Vitor Vilete <vitorgarcesvilete@gmail.com>
2024-01-17 17:14:51 -0300

    Adding simple movement to the player character

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 movementInput;
+    [SerializeField] private float moveSpeed = 7.0f;
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 movementDirection = new Vector3(movementInput.x, 0, movementInput.y);
+        transform.position += movementDirection * moveSpeed * Time.deltaTime;
+
+        float rotateSpeed = 10f;
+        //Use Slerp for lerping rotation
+        transform.forward = Vector3.Slerp(transform.forward, movementDirection, Time.deltaTime * rotateSpeed);
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+        if (context.performed)
+        {
+            Debug.Log(context.ToString());
+        }
+    }
+}

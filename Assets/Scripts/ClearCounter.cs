71cfd4fe19b408e608e9992c8ad71d569744a136
Vitor Vilete <vitorgarcesvilete@gmail.com>
2024-01-19 02:17:03 -0300

    Adding new prefabs; pickup and set down working

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,5 +8,30 @@ public class ClearCounter : BaseCounter, IKitchenObjectParent
 
     public override void Interact(Player player)
     {
+        if (!HasKitchenObject())
+        {
+            // There is no KitchenObject on the counter
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying a KitchenObject
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Player is not carrying anything
+            }
+        }
+        else
+        {
+            // There is KitchenObject on the counter
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+            } else
+            {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }

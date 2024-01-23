59d3062a152e70df1b7b71106238352729dab3f8
Vitor Vilete <vitorgarcesvilete@gmail.com>
2024-01-22 21:19:48 -0300

    Adding game states

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -20,6 +20,7 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
 
     private void Awake()
     {
@@ -75,7 +76,9 @@ public class DeliveryManager : MonoBehaviour
                 }
                 if (plateContentsMatchesRecipe) 
                 {
-                    // Player delivered the correct recipe                    
+                    // Player delivered the correct recipe
+                    successfulRecipesAmount++;
+
                     waitingRecipeSOList.RemoveAt(i);
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
                     OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
@@ -91,5 +94,10 @@ public class DeliveryManager : MonoBehaviour
     public List<RecipeSO> GetWaitingRecipeSOList()
     {
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount()
+    {
+        return successfulRecipesAmount;
     }
 }
